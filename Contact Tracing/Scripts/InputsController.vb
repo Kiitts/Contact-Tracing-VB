52bf0d8ff4dfb52879e2e7b28084d4b2a5951fe1
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-06-07 20:05:09 +0800

    Added new Controller on my Scripts folder, these is where the Inputs validation or control function can be find, moved my function that validate age and phone number in this script

---
+++ Contact Tracing/Scripts/InputsController.vb
@@ -0,0 +1,14 @@
+﻿Module InputsController
+    ''' <summary>
+    ''' Validate the digit input check if the key pressed is allowed
+    ''' and only handle the input if it isn't allowed
+    ''' </summary>
+    ''' <param name="length">This is the length of the digit allowed</param>
+    ''' <param name="input">This is the TextBox object you'll validate</param>
+    Public Sub ValidateDigitInput(input As TextBox, length As Integer, e As KeyPressEventArgs)
+        If (Not (e.KeyChar = ControlChars.Back Or
+            (Char.IsDigit(e.KeyChar) And input.TextLength < length))) Then
+            e.Handled = True
+        End If
+    End Sub
+End Module
--- Contact Tracing/mainForm.vb
+++ Contact Tracing/mainForm.vb
@@ -1,26 +1,14 @@
-﻿Public Class mainForm
+﻿Imports Contact_Tracing.InputsController
 
-    ''' <summary>
-    ''' Validate the digit input check if the key pressed is allowed
-    ''' and only handle the input if it isn't allowed
-    ''' </summary>
-    ''' <param name="length">This is the length of the digit allowed</param>
-    ''' <param name="input">This is the TextBox object you'll validate</param>
-    Private Sub ValidateDigitInput(input As TextBox, length As Integer, e As KeyPressEventArgs)
-        If (Not (e.KeyChar = ControlChars.Back Or
-            (Char.IsDigit(e.KeyChar) And input.TextLength < length))) Then
-            e.Handled = True
-        End If
-    End Sub
-
+Public Class mainForm
     Private Sub OnPressed(sender As Object, e As KeyPressEventArgs) Handles ageInput.KeyPress, numberInput.KeyPress
         Dim txtBox As TextBox = DirectCast(sender, TextBox)
-        ' If the event sender is ageInput validate input with validation length of only 2
         If (txtBox.Name = "ageInput") Then
-            ValidateDigitInput(txtBox, 2, e)
-            ' Else if numberInput validate input with validation length of 11
+            ' If the event sender is ageInput validate input with validation length of only 2
+            InputsController.ValidateDigitInput(txtBox, 2, e)
         Else
-            ValidateDigitInput(txtBox, 11, e)
+            ' Else if numberInput validate input with validation length of 11
+            InputsController.ValidateDigitInput(txtBox, 11, e)
         End If
     End Sub
 End Class
